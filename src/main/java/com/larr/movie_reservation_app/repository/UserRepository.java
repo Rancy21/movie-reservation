dfca4a3bdf1c0f3d0cca45a03ce0bf2e3a2f059d
Rancy21 <rancyontsandaga@gmail.com>
2025-11-24 22:08:41 +0200

    fix: add @Repository annotation

--- src/main/java/com/larr/movie_reservation_app/repository/UserRepository.java
+++ src/main/java/com/larr/movie_reservation_app/repository/UserRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.larr.movie_reservation_app.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
   Optional<User> findByEmail(String email);
 
