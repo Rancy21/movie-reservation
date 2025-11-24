b4be512cff1bec3c475e6b9604e9da045f26790a
Rancy21 <rancyontsandaga@gmail.com>
2025-11-24 19:51:48 +0200

    feat: created user repository

---
+++ src/main/java/com/larr/movie_reservation_app/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.larr.movie_reservation_app.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.larr.movie_reservation_app.model.User;
+
+public interface UserRepository extends JpaRepository<User, String> {
+  Optional<User> findByEmail(String email);
+
+  List<User> findAllByIsActiveTrue();
+}
