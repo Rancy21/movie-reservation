330555f44e9cde8315046046ba8010979754e206
Rancy21 <rancyontsandaga@gmail.om>
2025-11-24 13:44:23 +0200

    First commit

---
+++ src/main/java/com/larr/movie_reservation_app/MovieReservationAppApplication.java
@@ -0,0 +1,13 @@
+package com.larr.movie_reservation_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReservationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReservationAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/larr/movie_reservation_app/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.larr.movie_reservation_app;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MovieReservationAppApplication.class);
+	}
+
+}
