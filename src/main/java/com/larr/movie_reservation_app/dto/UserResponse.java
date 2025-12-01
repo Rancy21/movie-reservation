bee201477840bd86a6f19d9ff9aaeecdb01da3b5
Rancy21 <rancyontsandaga@gmail.com>
2025-12-01 20:34:11 +0200

    feat: create response object for user

---
+++ src/main/java/com/larr/movie_reservation_app/dto/UserResponse.java
@@ -0,0 +1,13 @@
+package com.larr.movie_reservation_app.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResponse {
+    private String id;
+    private String email;
+    private String name;
+    private String phoneNumber;
+}
