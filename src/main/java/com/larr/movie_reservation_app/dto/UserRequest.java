70fc886714c248c6b831e0e3d7551315506dd56d
Rancy21 <rancyontsandaga@gmail.com>
2025-12-01 20:20:16 +0200

    feat: create request dto for user

---
+++ src/main/java/com/larr/movie_reservation_app/dto/UserRequest.java
@@ -0,0 +1,13 @@
+package com.larr.movie_reservation_app.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRequest {
+    private String email;
+    private String name;
+    private String password;
+    private String phoneNumber;
+}
