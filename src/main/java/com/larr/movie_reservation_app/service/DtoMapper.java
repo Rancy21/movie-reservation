1a0bb1e59193154c86e00f5a973e1ad5a7b3d220
Rancy21 <rancyontsandaga@gmail.com>
2025-12-01 20:34:47 +0200

    feat: create a data transer mapper for dto entity conversion

---
+++ src/main/java/com/larr/movie_reservation_app/service/DtoMapper.java
@@ -0,0 +1,34 @@
+package com.larr.movie_reservation_app.service;
+
+import org.springframework.stereotype.Service;
+
+import com.larr.movie_reservation_app.dto.UserRequest;
+import com.larr.movie_reservation_app.dto.UserResponse;
+import com.larr.movie_reservation_app.model.Role;
+import com.larr.movie_reservation_app.model.User;
+
+@Service
+public class DtoMapper {
+    public User toUserEntity(UserRequest request) {
+        User user = new User();
+
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setPhoneNumber(request.getPhoneNumber());
+        user.setRole(Role.USER);
+
+        return user;
+    }
+
+    public UserResponse toUserResponse(User user) {
+        UserResponse response = new UserResponse();
+
+        response.setId(user.getId());
+        response.setName(user.getName());
+        response.setEmail(user.getEmail());
+        response.setPhoneNumber(user.getPhoneNumber());
+        return response;
+    }
+
+}
