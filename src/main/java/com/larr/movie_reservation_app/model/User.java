4f0665d3d87917753babab466f74bc699574f22e
Rancy21 <rancyontsandaga@gmail.com>
2025-12-04 18:57:15 +0200

    update: set 'USER' as default role

--- src/main/java/com/larr/movie_reservation_app/model/User.java
+++ src/main/java/com/larr/movie_reservation_app/model/User.java
@@ -26,5 +26,5 @@ public class User {
     private boolean isActive = true;
     @Enumerated(EnumType.STRING)
     @Column(name = "role")
-    private Role role;
+    private Role role = Role.USER;
 }
