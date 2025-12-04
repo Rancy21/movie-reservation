a4347156b97ed82fa52c36ea3d49d835209bbb85
Rancy21 <rancyontsandaga@gmail.com>
2025-12-04 18:57:42 +0200

    updatea: add filtering for movie

--- src/main/java/com/larr/movie_reservation_app/repository/MovieRepository.java
+++ src/main/java/com/larr/movie_reservation_app/repository/MovieRepository.java
@@ -1,8 +1,16 @@
 package com.larr.movie_reservation_app.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.larr.movie_reservation_app.model.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, String> {
+    @Query("select m from Movie m where " +
+            "(:genre is null or m.genre = :genre)" +
+            "and (:title is null or lower(m.title) ilike lower(concat('%', :title, '%')))")
+    Page<Movie> search(String genre, String title, Pageable pageable);
+
 }
