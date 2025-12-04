37ddbfaab2eeaf701a2e804e1a9d22f15dbce66d
Rancy21 <rancyontsandaga@gmail.com>
2025-12-04 19:11:16 +0200

    fix: add param reference

--- src/main/java/com/larr/movie_reservation_app/repository/MovieRepository.java
+++ src/main/java/com/larr/movie_reservation_app/repository/MovieRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.larr.movie_reservation_app.model.Movie;
 
@@ -11,6 +12,6 @@ public interface MovieRepository extends JpaRepository<Movie, String> {
     @Query("select m from Movie m where " +
             "(:genre is null or m.genre = :genre)" +
             "and (:title is null or lower(m.title) ilike lower(concat('%', :title, '%')))")
-    Page<Movie> search(String genre, String title, Pageable pageable);
+    Page<Movie> search(@Param("genre") String genre, @Param("title") String title, Pageable pageable);
 
 }
