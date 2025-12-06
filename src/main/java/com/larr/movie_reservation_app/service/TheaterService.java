5048f153e07ffc4746333b22c6074dc590d3ef82
Rancy21 <rancyontsandaga@gmail.com>
2025-12-06 20:18:58 +0200

    feat: create service layer for theater operations

---
+++ src/main/java/com/larr/movie_reservation_app/service/TheaterService.java
@@ -0,0 +1,61 @@
+package com.larr.movie_reservation_app.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.larr.movie_reservation_app.dto.TheaterDTO;
+import com.larr.movie_reservation_app.dto.TheaterDetailDTO;
+import com.larr.movie_reservation_app.dto.TheaterUpdateRequest;
+import com.larr.movie_reservation_app.exception.TheaterNotFoundException;
+import com.larr.movie_reservation_app.model.Theater;
+import com.larr.movie_reservation_app.repository.TheaterRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TheaterService {
+    private final TheaterRepository repository;
+
+    public Theater saveTheater(Theater theater) {
+        return repository.save(theater);
+    }
+
+    public Theater findTheater(String id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new TheaterNotFoundException("Theater with Id: " + id + " not found"));
+    }
+
+    public Theater updateTheater(String id, TheaterUpdateRequest request) {
+        Theater theater = findTheater(id);
+        theater.setName(request.name());
+        if (!request.city().isBlank())
+            theater.setCity(request.city());
+        if (!request.address().isBlank())
+            theater.setAddress(request.address());
+        theater.setTotalScreens(request.totalScreens());
+
+        return repository.save(theater);
+    }
+
+    public void deleteTheater(String id) {
+        Theater theater = findTheater(id);
+        theater.setActive(false);
+        repository.save(theater);
+        return;
+    }
+
+    public TheaterDetailDTO findTheaterById(String id) {
+        return repository.findByIdAsDto(id)
+                .orElseThrow(() -> new TheaterNotFoundException("Theater with Id: " + id + " not found"));
+    }
+
+    public Page<TheaterDTO> listAllActiveTheaters(Pageable pageable) {
+        return repository.listAllActive(pageable);
+    }
+
+    public Page<TheaterDTO> listAllTheater(Pageable pageable) {
+        return repository.listAll(pageable);
+    }
+}
