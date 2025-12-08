765fbc1f271f659fc0afff8046af85cd13d6c968
Rancy21 <rancyontsandaga@gmail.com>
2025-12-08 21:26:25 +0200

    update: removed mapstruct annotation and manually created TheaterMapper

--- src/main/java/com/larr/movie_reservation_app/service/mapper/TheaterMapper.java
+++ src/main/java/com/larr/movie_reservation_app/service/mapper/TheaterMapper.java
@@ -1,15 +1,43 @@
 package com.larr.movie_reservation_app.service.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
-
 import com.larr.movie_reservation_app.dto.TheaterCreateRequest;
 import com.larr.movie_reservation_app.dto.TheaterDTO;
 import com.larr.movie_reservation_app.model.Theater;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
-public interface TheaterMapper {
-    TheaterDTO toDto(Theater theater);
+@Component
+public class TheaterMapper {
 
-    Theater toEntity(TheaterCreateRequest request);
+    public TheaterDTO toDto(Theater theater) {
+        if (theater == null) {
+            return null;
+        }
+
+        String id = null;
+        String name = null;
+        String city = null;
+
+        id = theater.getId();
+        name = theater.getName();
+        city = theater.getCity();
+
+        TheaterDTO theaterDTO = new TheaterDTO(id, name, city);
+
+        return theaterDTO;
+    }
+
+    public Theater toEntity(TheaterCreateRequest request) {
+        if (request == null) {
+            return null;
+        }
+
+        Theater theater = new Theater();
+
+        theater.setName(request.name());
+        theater.setAddress(request.address());
+        theater.setCity(request.city());
+        theater.setTotalScreens(request.totalScreens());
+
+        return theater;
+    }
 }
