f8c3147373717e3565e8bf5df203cdd7c473f4dc
Rancy21 <rancyontsandaga@gmail.com>
2025-12-09 23:06:24 +0200

    update: added repository annotation

--- src/main/java/com/larr/movie_reservation_app/repository/ScreenRepository.java
+++ src/main/java/com/larr/movie_reservation_app/repository/ScreenRepository.java
@@ -1,9 +1,11 @@
 package com.larr.movie_reservation_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.larr.movie_reservation_app.model.Screen;
 
+@Repository
 public interface ScreenRepository extends JpaRepository<Screen, String> {
 
 }
