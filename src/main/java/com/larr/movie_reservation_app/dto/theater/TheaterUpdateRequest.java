0e2dfa1e9a5325f05599c9106c1db731ebeb30a1
Rancy21 <rancyontsandaga@gmail.com>
2025-12-10 12:53:25 +0200

    update: moved to theater package

--- src/main/java/com/larr/movie_reservation_app/dto/theater/TheaterUpdateRequest.java
+++ src/main/java/com/larr/movie_reservation_app/dto/theater/TheaterUpdateRequest.java
@@ -1,8 +1,8 @@
-package com.larr.movie_reservation_app.dto;
+package com.larr.movie_reservation_app.dto.theater;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 public record TheaterUpdateRequest(@NotBlank String name, String city, String address,
-        @Min(1) int totalScreens) {
+                @Min(1) int totalScreens) {
 }
