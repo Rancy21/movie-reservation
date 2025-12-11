21b4a42be9db308ce7dffff2058d9fd7ceaa65f8
Rancy21 <rancyontsandaga@gmail.com>
2025-12-11 14:14:59 +0200

    feat: exception to handle cases where a screen entity is not found

---
+++ src/main/java/com/larr/movie_reservation_app/exception/ScreenNotFoundException.java
@@ -0,0 +1,7 @@
+package com.larr.movie_reservation_app.exception;
+
+public class ScreenNotFoundException extends RuntimeException {
+    public ScreenNotFoundException(String message) {
+        super(message);
+    }
+}
