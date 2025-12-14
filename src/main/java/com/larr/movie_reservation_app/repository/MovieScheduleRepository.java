6ef35e1efc18d19c41aebdd60df1cc45e2437a25
Rancy21 <rancyontsandaga@gmail.com>
2025-12-14 14:12:50 +0200

    update: add method to fetch movie schedules by screen and movie and to fetch detailed information about movie schedule

--- src/main/java/com/larr/movie_reservation_app/repository/MovieScheduleRepository.java
+++ src/main/java/com/larr/movie_reservation_app/repository/MovieScheduleRepository.java
@@ -1,9 +1,45 @@
 package com.larr.movie_reservation_app.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import com.larr.movie_reservation_app.dto.movieSchedule.MovieScheduleDTO;
+import com.larr.movie_reservation_app.dto.movieSchedule.MovieScheduleDetailDTO;
 import com.larr.movie_reservation_app.model.MovieSchedule;
+import java.util.List;
+import java.util.Optional;
+import java.time.LocalDateTime;
 
+@Repository
 public interface MovieScheduleRepository extends JpaRepository<MovieSchedule, String> {
+        @Query("""
+                        select new com.larr.movie_reservation_app.dto.movieSchedule.MovieScheduleDTO(sc.id,sc.movie.title, sc.screen.screenNumber, sc.startTime)
+                        from MovieSchedule sc
+                        where sc.screen.id = ?1
+                        and (sc.startTime >= ?2 and sc.startTime < ?3)
+                                    """)
+        Page<MovieScheduleDTO> findByScreen(String screenId, LocalDateTime start, LocalDateTime end, Pageable pageable);
 
+        @Query("""
+                        select new com.larr.movie_reservation_app.dto.movieSchedule.MovieScheduleDTO(sc.id,sc.movie.title, sc.screen.screenNumber, sc.startTime)
+                         from MovieSchedule sc
+                         where sc.movie.id = ?1
+                         """)
+        List<MovieScheduleDTO> findByMovie(String movieId);
+
+        @Query("""
+                        select new com.larr.movie_reservation_app.dto.movieSchedule.MovieScheduleDetailDTO(
+                        sc.movie.title,
+                        sc.screen.theater.name,
+                        sc.screen.screenNumber,
+                        sc.startTime,
+                        sc.ticketPrice
+                        )
+                        From MovieSchedule sc
+                        Where sc.id = ?1
+                        """)
+        Optional<MovieScheduleDetailDTO> findScheduleDetail(String id);
 }
