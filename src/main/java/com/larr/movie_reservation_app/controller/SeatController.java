3337e564abf0a6f5f7022bc6280381fecb75a67e
Rancy21 <rancyontsandaga@gmail.com>
2025-12-21 23:15:21 +0200

    add lock-seat and find-available-seats endpoints in SeatController

--- src/main/java/com/larr/movie_reservation_app/controller/SeatController.java
+++ src/main/java/com/larr/movie_reservation_app/controller/SeatController.java
@@ -1,6 +1,9 @@
 package com.larr.movie_reservation_app.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,9 +11,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.larr.movie_reservation_app.dto.lock.LockRequest;
 import com.larr.movie_reservation_app.dto.seat.SeatCreateRequest;
 import com.larr.movie_reservation_app.dto.seat.SeatUpdateRequest;
+import com.larr.movie_reservation_app.model.SeatLock;
+import com.larr.movie_reservation_app.service.SeatLockService;
 import com.larr.movie_reservation_app.service.SeatService;
+import com.larr.movie_reservation_app.service.mapper.SeatLockMapper;
 import com.larr.movie_reservation_app.service.mapper.SeatMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -20,6 +27,9 @@ import lombok.RequiredArgsConstructor;
 public class SeatController {
     private final SeatService service;
     private final SeatMapper mapper;
+
+    private final SeatLockService seatLockService;
+    private final SeatLockMapper seatLockMapper;
 
     @PostMapping("/api/seats")
     public ResponseEntity<?> generateSeats(@RequestBody SeatCreateRequest request) {
@@ -41,4 +51,20 @@ public class SeatController {
         return ResponseEntity.ok(service.findSeatsByScreen(screenId));
     }
 
+    @PostMapping("api/seats/lock-seat")
+    public ResponseEntity<?> lockSeat(@RequestBody LockRequest request) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        SeatLock savedLock = seatLockService.lockSeat(request.seatId(), request.scheduleId(),
+                userDetails.getUsername());
+
+        return ResponseEntity.ok(seatLockMapper.toDTO(savedLock));
+    }
+
+    @GetMapping("/api/schedules/{scheduleId}/available-seats")
+    public ResponseEntity<?> findAvailableSeats(@PathVariable String scheduleId) {
+        return ResponseEntity.ok(service.finSeatAvailableSeats(scheduleId));
+    }
 }
